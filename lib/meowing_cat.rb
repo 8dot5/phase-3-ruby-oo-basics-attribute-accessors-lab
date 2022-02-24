61d14ac87eb47db90cd1a3ca0f404c6c845b863b
Chun Yi <8.5by11@gmail.com>
2022-02-24 12:26:45 -0500

    commit tree 0a13100768cb5ddca611e1b2e2ea081b797b98bf

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,35 @@
 ## code your solution here. 
+require 'pry'
+# class Person
+#     attr_reader :first_name, :last_name
+  
+#     def name=(full_name)
+#       first_name, last_name = full_name.split
+#       @first_name = first_name
+#       @last_name = last_name
+#     end
+  
+#     def name
+#       "#{@first_name} #{@last_name}".strip
+#     end
+  
+#   end
+#   binding.pry
+
+
+
+class Cat
+    attr_accessor :name
+
+    def meow
+        # puts "meow!"
+        puts "#{name} meow!"
+    end
+end
+# binding.pry
+
+#bo = Cat.new
+#bo.name = "Bo"
+#bo.meow = "meow!"
+
+
